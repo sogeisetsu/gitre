13d7be3f0c0f22047aaba81aa968099bdd55fc0d
苏月晟 <47493432+sogeisetsu@users.noreply.github.com>
2019-10-08 15:20:38 +0800

    取消key

--- lj/shiyan.py
+++ lj/shiyan.py
@@ -1,5 +1,5 @@
 import laji
-a="http://api.tianapi.com/txapi/lajifenlei/?key=63be040a50cf928e2c79e650fe57e7e6&word="
+a="http://api.tianapi.com/txapi/lajifenlei/?key=63&word="
 b="尸体"
 c=""
 laji.wenji(a,b,c)
